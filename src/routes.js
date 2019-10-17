5c786010b849d857528ff8da15de99cb36ff3155
Allef Douglas <allef.douglas@aftisolucoes.com.br>
2019-10-17 15:55:58 -0300

    First commit

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const UserController = require("./controllers/UserController");
+
+const routes = express.Router();
+
+routes.get("/users", UserController.index);
+// routes.get("/users/:id", UserController.show);
+routes.post("/users", UserController.store);
+// routes.put("/users/:id", UserController.update);
+// routes.delete("/users/:id", UserController.destroy);
+
+module.exports = routes;
